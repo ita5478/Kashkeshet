02b9585d5f9f7433d0d765d73b199aa3cdeedca4
ita5478 <ittai.paz@gmail.com>
2022-01-24 15:01:16 +0200

    Implementation: Add StringToByteArrayConverter that implements IConverter.

---
+++ KashkeshetExercise/Kashkeshet.Common/Implementations/StringToByteArrayConverter.cs
@@ -0,0 +1,19 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System.Text;
+
+namespace Kashkeshet.Common.Implementations.Converters
+{
+    public class StringToByteArrayConverter : IConverter<string, byte[]>
+    {
+        public string ConvertFrom(byte[] input)
+        {
+            return Encoding.ASCII.GetString(input);
+        }
+
+        public byte[] ConvertTo(string input)
+        {
+            return Encoding.ASCII.GetBytes(input);
+        }
+
+    }
+}
