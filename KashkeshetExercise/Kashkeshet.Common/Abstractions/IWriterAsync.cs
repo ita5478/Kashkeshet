70d11662e45e3ce6ba60541b7959e5d895ffb0f3
ita5478 <ittai.paz@gmail.com>
2022-01-24 19:07:43 +0200

    Addition: Add IWriterAsync abstraction.

---
+++ KashkeshetExercise/Kashkeshet.Common/Abstractions/IWriterAsync.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.Common.Abstractions
+{
+    public interface IWriterAsync <T>
+    {
+        Task WriteAsync(T data);
+    }
+}
