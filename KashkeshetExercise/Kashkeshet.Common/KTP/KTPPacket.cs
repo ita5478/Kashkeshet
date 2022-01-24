5ebb1d4f81ca2f1eac1e2d57250f8f430272e098
ita5478 <ittai.paz@gmail.com>
2022-01-24 19:10:04 +0200

    Modification: Modify KTPPacketHeader back to being KTPPacket.

---
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Common.KTP
+{
+    public class KTPPacket
+    {
+        public readonly KTPPacketType PacketType;
+
+        public readonly IDictionary<string, string> Headers;
+
+        public readonly byte[] Content;
+        public KTPPacket(KTPPacketType packetType, IDictionary<string, string> headers, byte[] content = null)
+        {
+            PacketType = packetType;
+            Headers = headers;
+            Content = content;
+        }
+    }
+}
