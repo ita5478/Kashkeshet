1acce8242c7a5a1f1a23195b26ee1405beeb2046
ita5478 <ittai.paz@gmail.com>
2022-01-24 15:59:18 +0200

    Fix: Fix main method, async is not supported in .net core 3.1.

--- KashkeshetExercise/Server.ConsoleUI/Program.cs
+++ KashkeshetExercise/Server.ConsoleUI/Program.cs
@@ -5,13 +5,13 @@ namespace Server.ConsoleUI
     internal class Program
     {
         const int PORT = 6666;
-        static async void Main(string[] args)
+        static void Main(string[] args)
         {
             Bootstrapper boot = new Bootstrapper();
 
             var listener = boot.Initialize();
 
-            await listener.ListenForClients(PORT);
+            listener.ListenForClients(PORT).GetAwaiter().GetResult();
         }
     }
 }
