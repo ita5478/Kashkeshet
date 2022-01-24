f253206b906e20a4b7976a8527de04896244c92e
ita5478 <ittai.paz@gmail.com>
2022-01-24 15:21:26 +0200

    Addition: Add ISocketStream abstraction.

---
+++ KashkeshetExercise/Server.BL/Abstractions/ISocketStream.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace Server.Core.BL.Abstractions
+{
+    public interface ISocketStream
+    {
+        Task<byte[]> ReadAsync(int bufferSize);
+
+        Task WriteAsync(byte[] buffer);
+
+        void Close();
+    }
+}
