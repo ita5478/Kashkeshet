daa14d84ad4e6d5b93c24616e412c028c6d4577d
ita5478 <ittai.paz@gmail.com>
2022-01-24 18:23:26 +0200

    Addition: Add IConnectionInitializer abstraction.

---
+++ KashkeshetExercise/Client.BL/Abstractions/IConnectionInitializer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Abstractions
+{
+    public interface IConnectionInitializer
+    {
+        public Task<bool> ConnectAsync(IPAddress ip, int port, string username);
+    }
+}
