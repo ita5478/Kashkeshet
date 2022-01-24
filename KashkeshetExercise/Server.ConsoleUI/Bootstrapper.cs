c8e2ca77ca4b584cc57c3acbf9687c2a3e28c931
ita5478 <ittai.paz@gmail.com>
2022-01-24 15:55:46 +0200

    Addition: Add Server bootstrapper.

---
+++ KashkeshetExercise/Server.ConsoleUI/Bootstrapper.cs
@@ -0,0 +1,19 @@
+﻿using Server.BL.Abstractions;
+using Server.BL.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.ConsoleUI
+{
+    public class Bootstrapper
+    {
+        public IClientListener Initialize()
+        {
+            var writer = new Implementation.ConsoleWriter();
+            var listener = new TcpSocketClientListener(writer);
+
+            return listener;
+        }
+    }
+}
