31de78cbf0eacebb639250dcbbdda6ef0c0d95ba
ita5478 <ittai.paz@gmail.com>
2022-01-24 18:21:20 +0200

    Addition: Add ClientSocketBase abstraction.

---
+++ KashkeshetExercise/Client.BL/Abstractions/ClientSocketBase.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Abstractions
+{
+    public abstract class ClientSocketBase
+    {
+        public bool IsConnected { get; protected set; }
+
+        public abstract Task<bool> ConnectAsync(IPAddress address, int port);
+
+        public abstract void Disconnect();
+
+        public abstract Task SendDataAsync(byte[] data);
+
+        public abstract Task<byte[]> ReceiveDataAsync();
+    }
+}
