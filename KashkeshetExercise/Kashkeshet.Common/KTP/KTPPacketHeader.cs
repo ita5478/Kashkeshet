a208950466199a78de2baf48e7417da81811448f
ita5478 <ittai.paz@gmail.com>
2022-01-24 15:03:13 +0200

    Modification: Modify KTPPacket to represent only the Header.

--- KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketHeader.cs
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketHeader.cs
@@ -4,19 +4,16 @@ using System.Text;
 
 namespace Kashkeshet.Common.KTP
 {
-    public class KTPPacket
+    public class KTPPacketHeader
     {
         public readonly KTPPacketType PacketType;
 
         public readonly IDictionary<string, string> Headers;
 
-        public readonly byte[] Content;
-
-        public KTPPacket(KTPPacketType packetType, IDictionary<string, string> headers, byte[] content = null)
+        public KTPPacketHeader(KTPPacketType packetType, IDictionary<string, string> headers, byte[] content = null)
         {
             PacketType = packetType;
             Headers = headers;
-            Content = content;
         }
     }
 }
