6460e0519359bbaace305559f8b9ec51f44c220a
ita5478 <ittai.paz@gmail.com>
2022-01-24 14:31:38 +0200

    Addition: Add KTPPacket DTO.

---
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketType.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Common.KTP
+{
+    public enum KTPPacketType
+    {
+        REQ,
+        RES,
+        PUSH
+    }
+}
