fd084d32794da4f4346ab166cb13adbda436ac4c
ita5478 <ittai.paz@gmail.com>
2022-01-24 14:58:49 +0200

    Implementation: Add HeadersParser that implements IParser.

---
+++ KashkeshetExercise/Kashkeshet.Common/Implementations/HeadersParser.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Common.Implementations
+{
+    public class HeadersParser : Abstractions.IParser<Dictionary<string, string>>
+    {
+        public Dictionary<string, string> Parse(string data)
+        {
+            Dictionary<string , string> result = new Dictionary<string,string>();
+
+            var headers = data.Split("\r\n");
+            foreach (var header in headers)
+            {
+                var parts = header.Split(':');
+
+                result[parts[0]] = parts[1];
+            }
+
+            return result;
+        }
+    }
+}
