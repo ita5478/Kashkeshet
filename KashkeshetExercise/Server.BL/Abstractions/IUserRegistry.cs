fba4bbcf6515aee181c11b1d7e3a94ce7547eb94
ita5478 <ittai.paz@gmail.com>
2022-01-25 20:43:56 +0200

    Addition: Add IUserRegistry abstraction.

---
+++ KashkeshetExercise/Server.BL/Abstractions/IUserRegistry.cs
@@ -0,0 +1,12 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System.Threading.Tasks;
+
+namespace Server.BL.Abstractions
+{
+    public interface IUserRegistry
+    {
+        Task Register(ISocketStream userStream);
+
+        ISocketStream GetUserStream(string userName);
+    }
+}
