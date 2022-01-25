5fe52af840140bb690a4b63c74e6ee462516016e
ita5478 <ittai.paz@gmail.com>
2022-01-25 20:32:08 +0200

    Addition: Add ServerConnectionFailureException.

---
+++ KashkeshetExercise/Client.BL/Exceptions/ServerConnectionFailureException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.BL.Exceptions
+{
+    public class ServerConnectionFailureException : Exception
+    {
+        public ServerConnectionFailureException() : base() { }
+
+        public ServerConnectionFailureException(string message) : base(message) { }
+    }
+}
