dc4b9296b3e760753c98ea0223f9ef9e747c7b60
ita5478 <ittai.paz@gmail.com>
2022-01-25 12:26:04 +0200

    Implementation:  Add KTPPacketReader that implements IReaderAsync.

---
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketReader.cs
@@ -0,0 +1,75 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.Common.KTP
+{
+    public class KTPPacketReader : IReaderAsync<KTPPacket>
+    {
+        private ISocketStream _socketStream;
+        private IConverter<string, byte[]> _stringToByteArrayConverter;
+        private IConverter<string, IDictionary<string, string>> _stringToHeadersConverter;
+
+        public KTPPacketReader(
+            ISocketStream socketStream, 
+            IConverter<string, byte[]> stringToByteArrayConverter, 
+            IConverter<string, IDictionary<string, string>> stringToHeadersConverter)
+        {
+            _socketStream = socketStream;
+            _stringToByteArrayConverter = stringToByteArrayConverter;
+            _stringToHeadersConverter = stringToHeadersConverter;
+        }
+
+        public async Task<KTPPacket> ReadAsync()
+        {
+            var mainHeader = await ReadMainHeaderAsync();
+
+            var partsOfHeader = mainHeader.Split(' ');
+
+            KTPPacketType packetType = (KTPPacketType) Enum.Parse(typeof(KTPPacketType), partsOfHeader[0]);
+            int headersLength = int.Parse(partsOfHeader[1]);
+
+            var bytes = await _socketStream.ReadAsync(headersLength);
+            var headers = _stringToHeadersConverter.ConvertTo(_stringToByteArrayConverter.ConvertFrom(bytes));
+
+            return new KTPPacket (packetType, headers, await ReadContentBytesAsync(headers));
+        }
+
+        private async Task<string> ReadMainHeaderAsync()
+        {
+            bool keepReading = true;
+            string header = string.Empty;
+            byte[] bytesReceived;
+
+            while (keepReading)
+            {
+                bytesReceived = await _socketStream.ReadAsync(1);
+
+                header += _stringToByteArrayConverter.ConvertFrom(bytesReceived);
+
+                if (header.EndsWith("\r\n"))
+                {
+                    keepReading = false;
+                }
+            }
+
+            return header;
+        }
+
+        private async Task<byte[]> ReadContentBytesAsync(IDictionary<string, string> headers)
+        {
+            byte[] bytes = null;
+
+            if (headers.ContainsKey("Content-Length"))
+            {
+                int contentLength = int.Parse(headers["Content-Length"]);
+
+                bytes = await _socketStream.ReadAsync(contentLength);
+            }
+
+            return bytes;
+        }
+    }
+}
