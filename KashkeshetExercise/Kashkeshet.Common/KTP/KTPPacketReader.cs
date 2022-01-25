bed7323746544e8088060eebb85999e498492fdc
ita5478 <ittai.paz@gmail.com>
2022-01-25 20:37:30 +0200

    Modification: Modify KTPPacketReeader.

--- KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketReader.cs
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketReader.cs
@@ -10,29 +10,31 @@ namespace Kashkeshet.Common.KTP
     {
         private ISocketStream _socketStream;
         private IConverter<string, byte[]> _stringToByteArrayConverter;
-        private IConverter<string, IDictionary<string, string>> _stringToHeadersConverter;
+        private IParser<IDictionary<string, string>> _headersParser;
 
         public KTPPacketReader(
             ISocketStream socketStream, 
             IConverter<string, byte[]> stringToByteArrayConverter, 
-            IConverter<string, IDictionary<string, string>> stringToHeadersConverter)
+            IParser<IDictionary<string, string>> headersParser)
         {
             _socketStream = socketStream;
             _stringToByteArrayConverter = stringToByteArrayConverter;
-            _stringToHeadersConverter = stringToHeadersConverter;
+            _headersParser = headersParser;
         }
 
         public async Task<KTPPacket> ReadAsync()
         {
             var mainHeader = await ReadMainHeaderAsync();
 
+            mainHeader =  mainHeader.Replace("\r\n", String.Empty);
             var partsOfHeader = mainHeader.Split(' ');
 
             KTPPacketType packetType = (KTPPacketType) Enum.Parse(typeof(KTPPacketType), partsOfHeader[0]);
             int headersLength = int.Parse(partsOfHeader[1]);
 
             var bytes = await _socketStream.ReadAsync(headersLength);
-            var headers = _stringToHeadersConverter.ConvertTo(_stringToByteArrayConverter.ConvertFrom(bytes));
+            string a = _stringToByteArrayConverter.ConvertFrom(bytes);
+            var headers = _headersParser.Parse(a);
 
             return new KTPPacket (packetType, headers, await ReadContentBytesAsync(headers));
         }
