85897d797e9b283321e89b00f93b20c1cc81d995
ita5478 <ittai.paz@gmail.com>
2022-01-25 20:45:31 +0200

    Modification: Modify the main in client.

--- KashkeshetExercise/Client.ConsoleUI/Program.cs
+++ KashkeshetExercise/Client.ConsoleUI/Program.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using Client.BL.Abstractions;
+using Client.BL.Exceptions;
+using Client.BL.Implementation;
+using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.Implementations;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Net;
 
 namespace Client.ConsoleUI
 {
     internal class Program
     {
+        const string NAME = "user1";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var stringToByteArrayConverter = new StringToByteArrayConverter();
+            var headerParser = new HeadersParser();
+
+            var initializer = new ServerConnectionInitializer(stringToByteArrayConverter, headerParser);
+
+            try
+            {
+                var socketStream = initializer.ConnectAsync(IPAddress.Parse("127.0.0.1"), 6666, NAME).GetAwaiter().GetResult();
+            }
+            catch (ServerConnectionFailureException)
+            {
+
+            }
+
+            Console.Read();
         }
     }
 }
