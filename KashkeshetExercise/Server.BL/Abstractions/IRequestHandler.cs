473e1901371155c9fec66441e5ff775fc6b1b967
ita5478 <ittai.paz@gmail.com>
2022-01-25 21:19:46 +0200

    Addition: Add IRequestHandler Abstraction.

---
+++ KashkeshetExercise/Server.BL/Abstractions/IRequestHandler.cs
@@ -0,0 +1,13 @@
+﻿using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.BL.Abstractions
+{
+    public interface IRequestHandler
+    {
+        Task HandleRequest(KTPPacket requestPacket);
+    }
+}
