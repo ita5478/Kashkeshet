aeac1d08110aea40cd5c3748a71507a4ef0b1227
ita5478 <ittai.paz@gmail.com>
2022-01-25 15:41:20 +0200

    Implementation: Add ServerConnectionInitializer that implements IConnectionInitializer.

---
+++ KashkeshetExercise/Client.BL/Implementation/ServerConnectionInitializer.cs
@@ -0,0 +1,24 @@
+﻿using Client.BL.Abstractions;
+using Kashkeshet.Common.Abstractions;
+using Server.BL.Implementation;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Implementation
+{
+    public class ServerConnectionInitializer : IConnectionInitializer
+    {
+        public async Task<ISocketStream> ConnectAsync(IPAddress ip, int port)
+        {
+            TcpClient tcpClient = new TcpClient();
+
+            await tcpClient.ConnectAsync(ip, port);
+
+            return new TcpSocketStream(tcpClient.GetStream());
+        }
+    }
+}
