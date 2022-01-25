a048b5be3ce169eb2235e70d20f163de89d6cc5b
ita5478 <ittai.paz@gmail.com>
2022-01-25 11:54:17 +0200

    Implementation: Add KTPPacketWriter that implements IWriterAsync.

---
+++ KashkeshetExercise/Kashkeshet.Common/KTP/KTPPacketWriter.cs
@@ -0,0 +1,42 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kashkeshet.Common.KTP
+{
+    public class KTPPacketWriter : IWriterAsync<KTPPacket>
+    {
+        private ISocketStream _socketStream;
+        private IConverter<string, byte[]> _converter;
+
+        public KTPPacketWriter(ISocketStream socketStream, IConverter<string, byte[]> converter)
+        {
+            _socketStream = socketStream;
+            _converter = converter;
+        }
+
+        public async Task WriteAsync(KTPPacket data)
+        {
+            string headers = string.Empty;
+
+            foreach(var header in data.Headers)
+            {
+                headers += $"{header.Key}: {header.Value}\r\n";
+            }
+
+            headers += "\r\n";
+
+            string packet = $"{data.PacketType.ToString()} {headers.Length}\r\n{headers}";
+            var packetBytes = _converter.ConvertTo(packet);
+
+            await _socketStream.WriteAsync(packetBytes);
+            
+            if(data.Content != null  &&  data.Content.Length > 0)
+            {
+                await _socketStream.WriteAsync(data.Content);
+            }
+        }
+    }
+}
