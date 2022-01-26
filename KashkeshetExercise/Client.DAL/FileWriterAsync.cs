f47aae613e0f36b664463e79a9190184d5ea80fe
ita5478 <ittai.paz@gmail.com>
2022-01-26 21:14:49 +0200

    Implementation: Add FileWriterAsync.

---
+++ KashkeshetExercise/Client.DAL/FileWriterAsync.cs
@@ -0,0 +1,30 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.DAL
+{
+    public class FileWriterAsync : IWriterAsync<byte[]>, IAsyncDisposable
+    {
+        private FileStream _fileStream;
+
+        public FileWriterAsync(string filepath, FileMode mode)
+        {
+            _fileStream = new FileStream(filepath, mode);
+        }
+
+        public async Task WriteAsync(byte[] data)
+        {
+            await _fileStream.WriteAsync(data);
+        }
+
+        async ValueTask IAsyncDisposable.DisposeAsync()
+        {
+            await _fileStream.FlushAsync();
+            await _fileStream.DisposeAsync();
+        }
+    }
+}
