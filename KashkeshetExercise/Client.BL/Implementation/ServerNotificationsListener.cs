3cdecd137894c8130b1e618cde3ca2709e016c50
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:45:16 +0200

    Implementation: Implement IServerNotificationsListener.

---
+++ KashkeshetExercise/Client.BL/Implementation/ServerNotificationsListener.cs
@@ -0,0 +1,42 @@
+﻿using Client.BL.Abstractions;
+using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.DTO;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Implementation
+{
+    public class ServerNotificationsListener : IServerNotificationsListener
+    {
+        private bool isListening;
+        private IConverter<ChatMessage, KTPPacket> _messageToPacketConverter;
+        private IWriter<string> _writer;
+
+        public ServerNotificationsListener(
+            IConverter<ChatMessage, KTPPacket> messageToPacketConverter, 
+            IWriter<string> writer)
+        {
+            _messageToPacketConverter = messageToPacketConverter;
+            _writer = writer;
+        }
+
+        public async Task ListenForServerNotifications(IReaderAsync<KTPPacket> packetsReader)
+        {
+            isListening = true;
+
+            while (isListening)
+            {
+                var packet = await packetsReader.ReadAsync();
+                
+                if(packet.PacketType is KTPPacketType.PUSH)
+                {
+                    var message = _messageToPacketConverter.ConvertFrom(packet);
+                    _writer.Write($"({message.ChatName}) {message.Sender}: {message.Content}");
+                }
+            }
+        }
+    }
+}
