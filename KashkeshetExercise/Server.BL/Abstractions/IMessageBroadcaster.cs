4f928a641dbf573e056ed057d03fdb81050f8f2c
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:39:50 +0200

    Addition: Add IMessageBroadcaster abstraction.

---
+++ KashkeshetExercise/Server.BL/Abstractions/IMessageBroadcaster.cs
@@ -0,0 +1,15 @@
+﻿using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.DTO;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.BL.Abstractions
+{
+    public interface IMessageBroadcaster
+    {
+        void BroadcastMessage(KTPPacket message, IEnumerable<IWriterAsync<KTPPacket>> recipients);
+    }
+}
