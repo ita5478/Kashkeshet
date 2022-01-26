9a59efeda1031f80d51e5e96c2cc3fde35d8e542
ita5478 <ittai.paz@gmail.com>
2022-01-26 17:44:12 +0200

    Addition: Add IUserStatusAnnouncer abstraction.

---
+++ KashkeshetExercise/Server.BL/Abstractions/IUserStatusAnnouncer.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.BL.Abstractions
+{
+    public interface IUserStatusAnnouncer
+    {
+        void AnnounceConnection(string userName);
+
+        void AnnounceDisconnection(string userName);
+    }
+}
