10ff61af750858948066605104d01a883e91658a
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:46:23 +0200

    Addition: Add IClientRunner abstraction.

---
+++ KashkeshetExercise/Client.BL/Abstractions/IClientRunner.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Abstractions
+{
+    public interface IClientRunner
+    {
+        Task Start(IPAddress address, int port, string username);
+    }
+}
