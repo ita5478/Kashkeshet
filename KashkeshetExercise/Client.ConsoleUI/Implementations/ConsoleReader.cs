fe0a3a831d00024b6149236bd11f9e47fc3580b7
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:49:01 +0200

    Implementation: Add ConsoleReader.

---
+++ KashkeshetExercise/Client.ConsoleUI/Implementations/ConsoleReader.cs
@@ -0,0 +1,15 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.ConsoleUI.Implementations
+{
+    public class ConsoleReader : IReader<string>
+    {
+        public string Read()
+        {
+            return Console.ReadLine();
+        }
+    }
+}
