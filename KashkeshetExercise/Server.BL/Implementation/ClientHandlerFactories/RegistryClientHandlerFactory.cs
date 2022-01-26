d4625d249ff5b86b4c2eefdc94b00fb94ec00596
ita5478 <ittai.paz@gmail.com>
2022-01-26 12:44:14 +0200

    Implementation: Implement RegistryClientHandlerFactory.

---
+++ KashkeshetExercise/Server.BL/Implementation/ClientHandlerFactories/RegistryClientHandlerFactory.cs
@@ -0,0 +1,36 @@
+﻿using Kashkeshet.Common.Abstractions;
+using Server.BL.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Server.BL.Implementation
+{
+    public class RegistryClientHandlerFactory : IClientHandlerFactory
+    {
+        private IParser<IDictionary<string, string>> _headersParser;
+        private IConverter<string, byte[]> _stringToByteArrayConverter;
+        private IClientHandlerFactory _clientHandlerFactory;
+        private IUserRegistry _usersRegistry;
+        private IWriter<string> _writer;
+
+        public RegistryClientHandlerFactory(
+            IParser<IDictionary<string, string>> headersParser, 
+            IConverter<string, byte[]> stringToByteArrayConverter, 
+            IClientHandlerFactory clientHandlerFactory, 
+            IUserRegistry usersRegistry, 
+            IWriter<string> writer)
+        {
+            _headersParser = headersParser;
+            _stringToByteArrayConverter = stringToByteArrayConverter;
+            _clientHandlerFactory = clientHandlerFactory;
+            _usersRegistry = usersRegistry;
+            _writer = writer;
+        }
+
+        public IClientHandler Create(ISocketStream clientStream)
+        {
+            return new RegisteryClientHandler(_usersRegistry, _writer, clientStream, _clientHandlerFactory, _headersParser, _stringToByteArrayConverter);
+        }
+    }
+}
