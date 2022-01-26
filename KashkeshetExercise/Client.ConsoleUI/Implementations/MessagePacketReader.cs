f3e681d31cce95d1b5f54842a3e4d005b437efa6
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:49:45 +0200

    Implementation: Add A message packet reader.

---
+++ KashkeshetExercise/Client.ConsoleUI/Implementations/MessagePacketReader.cs
@@ -0,0 +1,37 @@
+﻿using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.ConsoleUI.Implementations
+{
+    public class MessagePacketReader : IReader<KTPPacket>
+    {
+        private IWriter<string> _writer;
+        private IReader<string> _reader;
+        private IConverter<string, byte[]> _stringToByteArrayConverter;
+
+        public MessagePacketReader(IWriter<string> writer, IReader<string> reader, IConverter<string, byte[]> stringToByteArrayConverter)
+        {
+            _writer = writer;
+            _reader = reader;
+            _stringToByteArrayConverter = stringToByteArrayConverter;
+        }
+
+        public KTPPacket Read()
+        {
+            _writer.Write("Enter the message you want to send:");
+            string message = _reader.Read();
+
+            Dictionary<string, string> headers = new Dictionary<string, string>()
+            {
+                {"Date", DateTime.Now.ToLongDateString() },
+                {"Request-Type", "send" },
+                {"Content-Length", message.Length.ToString()}
+            };
+
+            return new KTPPacket(KTPPacketType.REQ, headers, _stringToByteArrayConverter.ConvertTo(message));
+        }
+    }
+}
