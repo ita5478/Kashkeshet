ecc5ac469935ba799a03ad26055d9a6ec1aa5614
ita5478 <ittai.paz@gmail.com>
2022-01-26 12:38:29 +0200

    Modification: Modify UserRegistry.

--- KashkeshetExercise/Server.BL/Implementation/UsersRegistry.cs
+++ KashkeshetExercise/Server.BL/Implementation/UsersRegistry.cs
@@ -11,77 +11,32 @@ namespace Server.BL.Implementation
 {
     public class UsersRegistry : IUserRegistry
     {
-        private ConcurrentDictionary<string, ClientHandler> _registeredClients;
-        private IParser<IDictionary<string, string>> _headersParser;
-        private IConverter<string, byte[]> _stringToByteArrayConverter;
-        private IWriter<string> _writer;
-        private IClientHandlerFactory _clientHandlerFactory;
-
-        public UsersRegistry(IClientHandlerFactory clientHandlerFactory, IWriter<string> writer)
+        private ConcurrentDictionary<string, IClientHandler> _registeredClients;
+        
+        public UsersRegistry()
         {
-            _registeredClients = new ConcurrentDictionary<string, ClientHandler>();
-            _headersParser = new HeadersParser();
-            _stringToByteArrayConverter = new StringToByteArrayConverter();
-            _writer = writer;
-            _clientHandlerFactory = clientHandlerFactory;
+            _registeredClients = new ConcurrentDictionary<string, IClientHandler>();
         }
 
-        public IWriterAsync<KTPPacket> GetUserWriter(string userName)
+        public IClientHandler GetUserHandler(string userName)
         {
-            return _registeredClients[userName].PacketsWriter;
+            return _registeredClients[userName];
         }
 
-        public async Task Register(ISocketStream userStream)
+        public bool IsUserRegistered(string username)
         {
-            var packetWriter = new KTPPacketWriter(userStream, _stringToByteArrayConverter);
-            var packetReader = new KTPPacketReader(userStream, _stringToByteArrayConverter, _headersParser);
-
-            Dictionary<string, string> headers = new Dictionary<string, string>()
-            {
-                { "Request-Type", "registry-identification" }
-            };
-
-            await packetWriter.WriteAsync(new KTPPacket(KTPPacketType.REQ, headers));
-
-            bool registryCompleted = false;
-            var retPacket = await packetReader.ReadAsync();
-            headers.Clear();
-
-            while (!registryCompleted)
-            {
-                if (retPacket.PacketType == KTPPacketType.RES)
-                {
-                    string username = retPacket.Headers["Identify-As"];
-                    if (!string.IsNullOrEmpty(username) && !_registeredClients.ContainsKey(retPacket.Headers["Identify-As"]))
-                    {
-                        registryCompleted = true;
-                        headers["Response-Type"] = "acknowledgement";
-                        headers["Response-Message"] = $"The user {username} was succussfully registered.";
-                        _writer.Write($"A new user by the name {username} has connected!");
-                        await packetWriter.WriteAsync(new KTPPacket(KTPPacketType.RES, headers));
-
-                        _registeredClients[username] = (ClientHandler)_clientHandlerFactory.Create(packetWriter, packetReader, username);
-                        StartClientHandler(username).Start();
-                    }
-                    else
-                    {
-                        headers["Response-Type"] = "error";
-                        headers["Response-Message"] = $"A user by the name {username} is already connected.";
-                        await packetWriter.WriteAsync(new KTPPacket(KTPPacketType.RES, headers));
-                        userStream.Close();
-                    }
-                }
-            }
+            return _registeredClients.ContainsKey(username);
         }
 
-        private async Task StartClientHandler(string userName)
+        public void Register(string userName, IClientHandler userHandler)
         {
-            await _registeredClients[userName].HandleClient();
-            ClientHandler clientHandler;
-            while(!_registeredClients.TryRemove(userName, out clientHandler))
-            {
-                await Task.Delay(100);
-            }
+            _registeredClients[userName] = userHandler;
+        }
+
+        public bool Unregister(string username)
+        {
+            IClientHandler handler;
+            return _registeredClients.TryRemove(username, out handler);
         }
     }
 }
