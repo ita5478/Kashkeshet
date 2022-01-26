3d3afe7e9dffad2b8dbb63b9b610648c2a1a6d1a
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:42:53 +0200

    Implementation: Add SendMessageRequestHandler.

---
+++ KashkeshetExercise/Server.BL/Implementation/RequestHandlers/SendMessageRequestHandler.cs
@@ -0,0 +1,42 @@
+﻿using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.DTO;
+using Kashkeshet.Common.KTP;
+using Server.BL.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Server.BL.Implementation.RequestHandlers
+{
+    public class SendMessageRequestHandler : IRequestHandler
+    {
+        private IMessageBroadcaster _messageBroadcaster;
+        private IUserRegistry _userRegistry;
+        private IConverter<ChatMessage, KTPPacket> _messageToPacketConverter;
+
+        public SendMessageRequestHandler(
+            IMessageBroadcaster messageBroadcaster, 
+            IUserRegistry userRegistry, 
+            IConverter<ChatMessage, KTPPacket> messageToPacketConverter)
+        {
+            _messageBroadcaster = messageBroadcaster;
+            _userRegistry = userRegistry;
+            _messageToPacketConverter = messageToPacketConverter;
+        }
+
+        public Task HandleRequest(KTPPacket requestPacket)
+        {
+            var message = _messageToPacketConverter.ConvertFrom(requestPacket);
+            List<IWriterAsync<KTPPacket>> recipientWriters = new List<IWriterAsync<KTPPacket>>();
+            
+            foreach(var recipient in _userRegistry.GetAllUsers())
+            {
+                recipientWriters.Add(_userRegistry.GetUserHandler(recipient).GetClientWriter());
+            }
+
+            _messageBroadcaster.BroadcastMessage(_messageToPacketConverter.ConvertTo(message), recipientWriters);
+            return Task.CompletedTask;
+        }
+    }
+}
