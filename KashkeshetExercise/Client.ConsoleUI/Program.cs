28890272f25eb57a726ef9daf3c4f6103f417c31
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:51:03 +0200

    Modification: Modify the client's main method.

--- KashkeshetExercise/Client.ConsoleUI/Program.cs
+++ KashkeshetExercise/Client.ConsoleUI/Program.cs
@@ -6,15 +6,14 @@ namespace Client.ConsoleUI
 {
     internal class Program
     {
-        const string NAME = "user1";
         static void Main(string[] args)
         {
             Bootstrapper boot = new Bootstrapper();
-            var initializer = boot.Initialize();
+            var clientRunner = boot.Initialize();
 
             try
             {
-                var socketStream = initializer.ConnectAsync(IPAddress.Parse("127.0.0.1"), 6666, NAME).GetAwaiter().GetResult();
+                clientRunner.Start(IPAddress.Parse("127.0.0.1"), 6666, args[0]).GetAwaiter().GetResult();
             }
             catch (ServerConnectionFailureException)
             {
