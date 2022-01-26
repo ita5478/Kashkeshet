4b28b672609732af7d92fa17e42d64d16ee3bc6f
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:46:52 +0200

    Implementation: Implement IClientRunner.

---
+++ KashkeshetExercise/Client.BL/Implementation/ClientRunner.cs
@@ -0,0 +1,50 @@
+﻿using Client.BL.Abstractions;
+using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.DTO;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Implementation
+{
+    public class ClientRunner : IClientRunner
+    {
+        private IConnectionInitializer _connectionInitializer;
+        private IServerNotificationsListener _serverNotificationsListener;
+        private IConverter<string, byte[]> _stringToByteArrayConverter;
+        private IParser<IDictionary<string, string>> _headersParser;
+        private IReader<KTPPacket> _inputReader;
+
+        public ClientRunner(
+            IConnectionInitializer connectionInitializer, 
+            IServerNotificationsListener serverNotificationsListener, 
+            IConverter<string, byte[]> stringToByteArrayConverter, 
+            IParser<IDictionary<string, string>> headersParser, 
+            IReader<KTPPacket> inputReader)
+        {
+            _connectionInitializer = connectionInitializer;
+            _serverNotificationsListener = serverNotificationsListener;
+            _stringToByteArrayConverter = stringToByteArrayConverter;
+            _headersParser = headersParser;
+            _inputReader = inputReader;
+        }
+
+        public async Task Start(IPAddress address, int port, string username)
+        {
+            var socket = await _connectionInitializer.ConnectAsync(address, port, username);
+            _serverNotificationsListener.ListenForServerNotifications(new KTPPacketReader(socket, _stringToByteArrayConverter, _headersParser));
+
+            var packetWriter = new KTPPacketWriter(socket, _stringToByteArrayConverter);
+
+            while (true)
+            {
+                var packet = _inputReader.Read();
+                packet.Headers["Sender"] = username;
+                await packetWriter.WriteAsync(packet);
+            }
+        }
+    }
+}
