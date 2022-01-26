f96769db4d07aa2b260a8b10950f75890ada11de
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:44:02 +0200

    Addition: Add IServerNotificationsListener abstraction.

---
+++ KashkeshetExercise/Client.BL/Abstractions/IServerNotificationsListener.cs
@@ -0,0 +1,14 @@
+﻿using Kashkeshet.Common.Abstractions;
+using Kashkeshet.Common.KTP;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.BL.Abstractions
+{
+    public interface IServerNotificationsListener
+    {
+        Task ListenForServerNotifications(IReaderAsync<KTPPacket> packetsReader);
+    }
+}
