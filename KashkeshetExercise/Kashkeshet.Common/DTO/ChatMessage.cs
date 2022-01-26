8203e8c149b06be770dfae684b73dd1d80d5e36a
ita5478 <ittai.paz@gmail.com>
2022-01-26 10:53:48 +0200

    Addition: Add ChatMessage DTO.

---
+++ KashkeshetExercise/Kashkeshet.Common/DTO/ChatMessage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Kashkeshet.Common.DTO
+{
+    public class ChatMessage
+    {
+        public readonly string Sender;
+        public readonly string ChatName;
+        public readonly DateTime TimeOfSending;
+        public readonly string Content;
+
+        public ChatMessage(string sender, string chatName, DateTime timeOfSending, string content)
+        {
+            Sender = sender;
+            ChatName = chatName;
+            TimeOfSending = timeOfSending;
+            Content = content;
+        }
+    }
+}
