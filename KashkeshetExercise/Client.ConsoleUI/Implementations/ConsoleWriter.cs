298877dcb972530dbe0367abe353f71225a3f536
ita5478 <ittai.paz@gmail.com>
2022-01-26 16:49:17 +0200

    Implementation: Add ConsoleWriter.

---
+++ KashkeshetExercise/Client.ConsoleUI/Implementations/ConsoleWriter.cs
@@ -0,0 +1,15 @@
+﻿using Kashkeshet.Common.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Client.ConsoleUI.Implementations
+{
+    public class ConsoleWriter : IWriter<string>
+    {
+        public void Write(string data)
+        {
+            Console.WriteLine(data);
+        }
+    }
+}
